fe9ca291d53f42032473bcc9d31f3db5f8488f06
hudda siddique <huddasiddique@huddas-Air.home>
2020-08-10 15:15:17 -0400

    finishing up

---
+++ SQLZOO.NET:BASICS/1.sql
@@ -0,0 +1,7 @@
+/*The example uses a WHERE clause to show the population of 'France'. Note that strings (pieces of text that are data) should be in 'single quotes';
+
+Modify it to show the population of Germany Changing the name to Germany will give the correct answer*/
+
+
+SELECT population FROM world
+  WHERE name = 'Germany'
---
+++ SQLZOO.NET:NAMES/2.sql
@@ -0,0 +1,5 @@
+
+/*
+Find the countries that end with y */
+SELECT name FROM world
+  WHERE name LIKE '%y'
